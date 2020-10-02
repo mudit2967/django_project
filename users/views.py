186d2907eecdce92207b158119dbf5dfedcecf6e
Ankit Bansal <35359451+ankit986@users.noreply.github.com>
2020-10-02 23:04:37 +0530

    An error might occur when you register new user
    In message.success(), you mistakenly added a random extra character that might result in error when you register new user.

--- users/views.py
+++ users/views.py
@@ -11,7 +11,7 @@ def register(request):
 		if form.is_valid():
 			form.save()
 			username=form.cleaned_data.get('username')
-			messages.success(request, f'Your Account Has Been Created,Kindly Login With Your Credential')
+			messages.success(request, 'Your Account Has Been Created,Kindly Login With Your Credential')
 			return redirect('login')
 		else :
 			messages.failure(request, f'Something went wrong try again ')
